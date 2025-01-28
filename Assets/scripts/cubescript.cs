84200bddd5f88a729745e84af446492ad841e3e3
imax.peas <imax.peas@gmail.com>
2025-01-28 21:26:11 +0300

    lesson 7 pinball

---
+++ Assets/scripts/cubescript.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cubescript : MonoBehaviour
+{
+    [SerializeField] [Range(0, 100)] float speed = 5f; 
+    [Space]
+    [SerializeField] GameObject blin;
+    [Space]
+    [SerializeField] string cubescriptName = "VENOMCUBE";
+    // Start is called before the first frame update
+    void Start()
+    {
+     gameObject.name = cubescriptName;
+     blin.name = "blinchik";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
